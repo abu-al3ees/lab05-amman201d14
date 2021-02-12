ef085cfa81217ae37a347c341811a8c70bdcae9c
abu-al3ees <malkhazali83@gmail.com>
2021-02-13 00:14:14 +0200

    multy array solved

--- app.js
+++ app.js
@@ -105,10 +105,13 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) {
   //eslint-disable-line
+  let x = multiply(multArr[0],multArr[1])[0];
+  let m=  multiply(x,multArr[2])[0];
+  return[m,`The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${m}.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
